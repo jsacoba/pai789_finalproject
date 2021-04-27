ed24499116025915b624fd94616a32243d3e3578
jonnel acoba <jsacoba@syr.edu>
2021-04-27 14:22:22 -0400

    update

--- script5_analyze/1.dist_plot_wri/dist_wri.py
+++ script5_analyze/1.dist_plot_wri/dist_wri.py
@@ -33,6 +33,7 @@ fig.savefig('dist_wri_region.png', dpi=300)
 fig, ax1 = plt.subplots()
 sns.set_style('darkgrid')
 sns.kdeplot(data=plots_data, shade=True, ax=ax1, x="2020", hue = "Risk Level")
+plots_data
 ax1.axvline(linewidth=2, color='r', linestyle="dotted")
 ax1.set_xlabel('World Risk Index')
 fig.tight_layout()
