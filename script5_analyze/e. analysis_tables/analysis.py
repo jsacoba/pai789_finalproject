652e907a3348a40559da64c284aaeceec390d7d5
jonnel acoba <jsacoba@syr.edu>
2021-04-27 22:30:25 -0400

    Update

--- script5_analyze/e. analysis_tables/analysis.py
+++ script5_analyze/e. analysis_tables/analysis.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Read input file.
 
@@ -17,4 +20,14 @@ rich = analyze.nlargest(10, ['GDP'])
 poor = analyze.nsmallest(10, ['GDP'])
 
 extremes = pd.concat([rich, poor], axis = 0)
+
+fig, ax1 = plt.subplots()
+region_scatter =sns.scatterplot(x="2020", y="Economic Region", 
+                         hue='Economic Region', data=analyze, s=15, 
+                         palette="muted")
+sns.set_style('darkgrid')
+region_scatter.set(xlabel='World Risk Index', ylabel='Economic Region')
+region_scatter.set_title("Economic Region vs. World Risk Index")
+fig.tight_layout()
+fig.savefig('scatter_region.png', dpi=300)
                    
