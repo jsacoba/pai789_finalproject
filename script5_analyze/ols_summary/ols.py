c8f585182dd8eb0797a7a7709f945d9052032507
jonnel acoba <jsacoba@syr.edu>
2021-05-05 14:45:55 -0400

    Update

--- script5_analyze/ols_summary/ols.py
+++ script5_analyze/ols_summary/ols.py
@@ -1,15 +1,15 @@
-# Import modules
+# 1. Import modules
 
 import pandas as pd
 import statsmodels.api as sm
 
-# Read input file.
+# 2. Read input file.
 
 analyze = pd.read_csv('combined_clean.csv')
 
 # A. Test the Correlation: WRI and EPI.
 
-# Generate and print correlation coefficient (rounded to three decimal places).
+# 3. Generate and print correlation coefficient (rounded to three decimal places).
 
 print('\Correlation Coefficient, r (WRI vs. EPI):', 
       round(analyze['2020'].corr(analyze['EPI.new']), 3))
@@ -20,31 +20,32 @@ print('\Correlation Coefficient, r (Vulnerability vs. EPI):',
 
 # A. Regress WRI and EPI.
 
+# 4. Define dependent variable.
+
 y = analyze['2020']
 
-# Define independent variable
+# 5. Define independent variable
 
 x = analyze[['EPI.new']]
 
-# Add constant to independent variable.
+# 6. Add constant to independent variable.
 
 x = sm.add_constant(x)
 
-# Fit linear regression model.
+# 7. Fit linear regression model.
 
 model = sm.OLS(y, x).fit(cov_type='HC1')
 
-# Print model summary.
+# 8. Print model summary.
 
 print(model.summary())
 
-# Save regression results.
+# 9. Save regression results.
 
 with open('ols_summary_epi_wri.txt', 'w') as fh:
           fh.write(model.summary().as_text())
           
-# B. Regress Exposure and EPI.
-
+# B. Regress Exposure and EPI. (10)
 y = analyze['Exposure']
 
 # Define independent variable
@@ -68,7 +69,7 @@ print(model.summary())
 with open('ols_summary_epi_exposure.txt', 'w') as fh:
           fh.write(model.summary().as_text())
 
-# C. Regress Vulnerability and EPI.
+# C. Regress Vulnerability and EPI. (11)
 
 y = analyze['Vulnerability']
 
